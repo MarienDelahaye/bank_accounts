2ce6d365312e44b6792fe652ba8e8fbde9ddf41d
Delahaye Marien <delahaye.marien@gmail.com>
2017-11-09 09:17:49 +0100

    Added php boilerplate without replacing my index.php

--- index.php
+++ index.php
@@ -81,18 +81,6 @@ footer {
   <body>
   <header class="mui-appbar mui--z1">
   <div class="mui-container">
-  <div class="mui-dropdown">
-  <button class="mui-btn mui-btn--primary" data-mui-toggle="dropdown">
-    Dropdown
-    <span class="mui-caret"></span>
-  </button>
-  <ul class="mui-dropdown__menu">
-    <li><a href="#">Option 1</a></li>
-    <li><a href="#">Option 2</a></li>
-    <li><a href="#">Option 3</a></li>
-    <li><a href="#">Option 4</a></li>
-  </ul>
-</div>
   <table>
     <tr class="mui--appbar-height">
       <td class="mui--text-title">Bank accounts</td>
